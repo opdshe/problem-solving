ae8d8a8b001d6668b2211a61c590b212e2f66daf
opdshe <opdshe@naver.com>
2021-08-25 11:36:33 +0900

    add: 피보나치(dp)

--- 재귀/피보나치수.java
+++ 재귀/피보나치수.java
@@ -1,5 +1,6 @@
 package 재귀;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 피보나치수 {
@@ -7,14 +8,18 @@ public class 피보나치수 {
 
 	public static void main(String[] args) {
 		int target = scanner.nextInt();
-		int answer = fibonacci(target);
+		int[] dp = new int[target + 1];
+		Arrays.fill(dp, -1);
+		dp[0] = 0;
+		dp[1] = 1;
+		int answer = fibonacci(dp, target);
 		System.out.println(answer);
 	}
 
-	private static int fibonacci(int target) {
-		if(target == 0 || target == 1){
-			return target;
+	private static int fibonacci(int[] dp, int target) {
+		if (dp[target] != -1) {
+			return dp[target];
 		}
-		return fibonacci(target - 1) + fibonacci(target - 2);
+		return dp[target] = fibonacci(dp, target - 1) + fibonacci(dp, target - 2);
 	}
 }
